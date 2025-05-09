95e88248d7328ce369454fadf216e8652ecdf80b
Lionel Martin <lionel.martin@lyra.com>
2025-05-09 15:09:50 +0200

    Add HTML simple variable parser

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,28 @@
 // vite.config.js
 import { sync } from "glob";
+import posthtml from '@vituum/vite-plugin-posthtml'
+import fs from 'fs';
+import path from 'path';
+import expressions from 'posthtml-expressions'
+
+const rawdata = fs.readFileSync(path.resolve(__dirname, 'variables.json'))
+const variables = JSON.parse(rawdata)
+
+global.vars = variables
 
 export default {
   root: "./src",
   build: {
     outDir: "../dist",
-      emptyOutDir: true,
+    emptyOutDir: true,
     rollupOptions: {
         input: {
             input: sync("./src/**/*.html".replace(/\\/g, "/")),
-        }
-    },
+            }
+        },
   },
+  plugins: [
+      posthtml({
+            plugins: [expressions({ locals: variables })]
+        })  ]
 };
